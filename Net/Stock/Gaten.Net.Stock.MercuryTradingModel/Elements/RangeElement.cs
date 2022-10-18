9c7a8a99d2924120c46b3ba9236e203210514449
Gaten <dongbin300@gmail.com>
2022-10-18 09:27:26 +0900

    Add range element and trade element concept

---
+++ Net/Stock/Gaten.Net.Stock.MercuryTradingModel/Elements/RangeElement.cs
@@ -0,0 +1,21 @@
+﻿using Gaten.Net.Stock.MercuryTradingModel.Interfaces;
+
+namespace Gaten.Net.Stock.MercuryTradingModel.Elements
+{
+    public class RangeElement : IElement
+    {
+        public decimal StartValue { get; set; }
+        public decimal EndValue { get; set; }
+
+        public RangeElement(decimal startValue, decimal endValue)
+        {
+            StartValue = startValue;
+            EndValue = endValue;
+        }
+
+        public override string ToString()
+        {
+            return StartValue.ToString() + "~" + EndValue.ToString();
+        }
+    }
+}
---
+++ Net/Stock/Gaten.Net.Stock.MercuryTradingModel/Enums/TradeElementType.cs
@@ -0,0 +1,9 @@
+﻿namespace Gaten.Net.Stock.MercuryTradingModel.Enums
+{
+    public enum TradeElementType
+    {
+        None = 0,
+        roe = 10,
+        upnl = 11
+    }
+}
