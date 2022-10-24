924d057db59dd07c64f5f9dc3f97a0f38eb0adcb
Gaten <dongbin300@gmail.com>
2022-10-24 10:46:50 +0900

    Modify RI Histogram

--- Net/Stock/Gaten.Net.Stock.MercuryTradingModel/Indicators/IndicatorExtension.cs
+++ Net/Stock/Gaten.Net.Stock.MercuryTradingModel/Indicators/IndicatorExtension.cs
@@ -11,7 +11,8 @@ namespace Gaten.Net.Stock.MercuryTradingModel.Indicators
             IList<RiResult> result = new List<RiResult>();
             var quoteList = quotes.ToList();
 
-            for (int i = 0; i < quoteList.Count; i++)
+            result.Add(new RiResult(quoteList[0].Date, 0));
+            for (int i = 1; i < quoteList.Count; i++)
             {
                 var average = quoteList.CloseAverage(i, period);
                 var ri = (quoteList[i].Close.Convert<double>() - average) / average * 1000;
