030322eae7caf7bcb10a1506545092c383cc06e3
Gaten <dongbin300@gmail.com>
2022-10-24 12:09:36 +0900

    Add Kelly criterion

---
+++ Net/Stock/Gaten.Net.Stock.MercuryTradingModel/Maths/KellyCriterion.cs
@@ -0,0 +1,15 @@
+﻿namespace Gaten.Net.Stock.MercuryTradingModel.Maths
+{
+    public class KellyCriterion
+    {
+        public static double Calculate(double probability, double proportion)
+        {
+            if (proportion <= 0)
+            {
+                return 0;
+            }
+
+            return (probability * (proportion + 1) - 1) / proportion;
+        }
+    }
+}
